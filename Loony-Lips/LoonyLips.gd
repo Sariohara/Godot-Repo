13aa33c93b881b0ed66c48112d64b9ea013f7c6d
Michael Eaton <mikeye2012@gmail.com>
2018-12-06 21:57:08 -0800

    Udemy_Godot_Arrays completed

--- Loony-Lips/LoonyLips.gd
+++ Loony-Lips/LoonyLips.gd
@@ -1,4 +1,6 @@
 extends Node2D
 
 func _ready():
-	pass
+	var prompt = ["Akira", "Lilly", "fun"]
+	var story = "Once upon a time in %s City. There was a girl named %s who was having %s at the Arcade."
+	print(story % prompt)
